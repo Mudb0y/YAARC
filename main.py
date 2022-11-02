8d065f41e4a8fb849577bbe4f78645269d315d33
Stanislaw Przedzinkowski <48333171+Mudb0y@users.noreply.github.com>
2022-11-02 22:19:53 +0100

    Fix Authlib

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 import os
-import pyglet
+import wx
 import praw
 from lib import speech
 from lib import authlib
 
-window = pyglet.window.Window()
+#app = wx.App()
 
-if not os.path.isfile ("token.auth"):
-    print ("No saved token is found.\nWaiting for authorisation...")
-    speech.speak ("It looks like you're running YAARC for the first time! For the full functionality of this app to work, you need to authorise the program with your Reddit account. Press enter to open the authorisation website in your default browser.")    
-    @window.event
-    def on_key_press(symbol, modifiers):
-        if symbol == pyglet.window.key.ENTER:
-            authlib.get_token()
-else:
-    speech.speak ("Welcome to YAARC!")
+#frame = wx.Frame(None, title="YAARC")
+#frame.Show()
 
-pyglet.app.run()
+
+
+#app.MainLoop()
+
+
+print ("Auth test...")
+authlib.authenticate()
