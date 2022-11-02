45fe723a52cae4785b6e77115aff0f02ae384a94
Stanislaw Przedzinkowski <48333171+Mudb0y@users.noreply.github.com>
2022-11-02 10:51:39 +0100

    Mostly fix authentication.

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import os
-from . import speech
+from lib import speech
 import pyglet
 import praw
 
 window = pyglet.window.Window()
+
+if not os.path.isfile ("auth.token"):
+    from lib import oauth
 
 pyglet.app.run()
