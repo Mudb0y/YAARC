888a4de0466db8d6beaa89ad7d81ab331e49ac92
Stanislaw Przedzinkowski <48333171+Mudb0y@users.noreply.github.com>
2022-11-02 12:29:59 +0100

    Implement saving of the refresh token in to a file.

--- main.py
+++ main.py
@@ -6,7 +6,14 @@ from lib import oauth
 
 window = pyglet.window.Window()
 
-if not os.path.isfile ("auth.token"):
-    oauth.authorise ()
-    
+if not os.path.isfile ("token.auth"):
+    print ("No saved token is found.\nWaiting for authorisation...")
+    speech.speak ("It looks like you're running YAARC for the first time! For the full functionality of this app to work, you need to authorise the program with your Reddit account. Press enter to open the authorisation website in your default browser.")    
+    @window.event
+    def on_key_press(symbol, modifiers):
+        if symbol == pyglet.window.key.ENTER:
+            oauth.authorise()
+else:
+    speech.speak ("Welcome to YAARC!")
+
 pyglet.app.run()
