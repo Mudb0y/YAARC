3550f5cf2e03ec15e28a97092bb696b8ae7ab251
Stanislaw Przedzinkowski <48333171+Mudb0y@users.noreply.github.com>
2022-11-04 14:08:14 +0100

    Resolve some UI issues.

--- main.py
+++ main.py
@@ -5,3 +5,13 @@ from lib import speech
 from lib import authlib
 from lib import ui
 
+class App(wx.App):
+    def OnInit(self):
+        self.frame = ui.main_window(None, wx.ID_ANY, "")
+        self.SetTopWindow(self.frame)
+        self.frame.Show()
+        return True
+
+if __name__ == "__main__":
+    app = App(0)
+    app.MainLoop()
