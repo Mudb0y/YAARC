21e090adb29174efac969319dc301133fc548316
Stanislaw Przedzinkowski <48333171+Mudb0y@users.noreply.github.com>
2022-11-04 22:03:17 +0100

    Finish authorisation UI. YAY!

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
+import os
 import wx
 from lib import authlib
-from lib import ui
+from lib import ui, authui
 
 # Main WX event-loop.
 class App(wx.App):
     def OnInit(self):
+
         self.frame = ui.main_window(None, wx.ID_ANY, "")
         self.SetTopWindow(self.frame)
         self.frame.Show()
+        if not os.path.isfile("tokan.auth"):
+            auth_dialog = authui.no_token(self.frame)
+            auth_dialog.ShowModal()
+        else:
+            authlib.authorise()
         return True
 
 if __name__ == "__main__":
