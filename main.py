72ebe9e9829895356d3b723eff6201a6da37626f
Stanislaw Przedzinkowski <48333171+Mudb0y@users.noreply.github.com>
2022-11-04 15:00:14 +0100

    Misc changes.

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-import os
-import praw
 import wx
-from lib import speech
-from lib import authlib
 from lib import ui
 
+# Main WX event-loop.
 class App(wx.App):
     def OnInit(self):
         self.frame = ui.main_window(None, wx.ID_ANY, "")
         self.SetTopWindow(self.frame)
         self.frame.Show()
         return True
+
+ui.Welcome(parent)
 
 if __name__ == "__main__":
     app = App(0)
